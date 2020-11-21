20e797e22d8dc5faa9ebdc4ce6cdca8aa8d36765
neavl <nevinitsin@mail.ru>
2020-11-21 08:59:13 +0300

    19.11.2020

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let f='77jju7';
+let App = function() {
+    console.log('App: инициализация');
+}
+
+let app = new App();
+console.log(f);
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+//const path = require('path');
+
+//var webpack = require("webpack");
+//let webpackCli = require("webpack-cli");
+module.exports = {
+    entry: './src/index.js',
+    output:
+    {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    }
+
+
+}
